607e08e89011017ad2fa20531555c2fafd295363
Hrishi Logani <118128840+loganihrishi@users.noreply.github.com>
2023-08-02 13:13:07 -0700

    used OOP instead

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ screen_width = 700
 screen_height = 500
 background = (255, 255, 255)
 
-# player attributes
-player_x = 50
-player_y = 440
-delta_y = 0
-gravity = 1
-score = 0
-player_color = (255, 0, 0)
+# player class
+class Player:
+    def __init__(self):
+        self.x = 50
+        self.y = 440
+        self.delta_y = 0
+        self.gravity = 1
+        self.color = (255, 0, 0)
+
+    def jump(self):
+        if self.y == 440:  # Check if player is on the floor to jump
+            self.delta_y = 15  # Adjust the jump strength as per your requirement
+
+    def update(self):
+        if 0 < self.delta_y or self.y < screen_height:
+            self.y -= self.delta_y
+            self.delta_y -= self.gravity
+
+        if self.y > 440:  # Check if player has reached the floor
+            self.y = 440
 
 # starting out the window
 pygame.init()
@@ -20,29 +33,24 @@ pygame.display.set_caption("Infinite Runner")
 fps = 50
 timer = pygame.time.Clock()
 
+player = Player()  # Create the player instance
+
 isRunning = True
 while isRunning:
     timer.tick(fps)
     screen.fill(background)
     floor = pygame.draw.rect(screen, (0, 0, 0), [2, 440, screen_width, 5])
-    player = pygame.draw.rect(screen, player_color, [player_x, player_y, 10, 20])
+    player_rect = pygame.draw.rect(screen, player.color, [player.x, player.y, 10, 20])
+
     # checking the events during the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_y == 440:
-                delta_y = 15
+            if event.key == pygame.K_SPACE:
+                player.jump()
 
-                # ensuring that the player stays in the bounds
-    if 0 < delta_y or player_y < screen_height:
-        player_y -= delta_y
-        delta_y -= gravity
-
-    if player_y > 440:
-        player_y = 440
-
-    # ensuring the player's x coordinate
+    player.update()
 
     # updating the display
     pygame.display.flip()
