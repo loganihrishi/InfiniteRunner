62c7883efc091bb8ed82745ac5033582bbc19058
Hrishi Logani <118128840+loganihrishi@users.noreply.github.com>
2023-08-05 20:26:57 -0700

    added HandDetection

--- detector.py
+++ detector.py
@@ -1,3 +1,31 @@
-class HandDetector:
-    def __init__(self):
-        pass
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+class PalmDetector:
+    def __init__(self, detectionCon=0.8, maxHands=1):
+        self.detector = HandDetector(detectionCon=detectionCon, maxHands=maxHands)
+
+    def is_palm_extended(self):
+        ret, frame = cap.read()
+        hands, img = self.detector.findHands(frame)
+        if hands:
+            lmlist = hands[0]
+            fingerUp = self.detector.fingersUp(lmlist)
+            return all(fingerUp)
+        return False
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    finger_detector = PalmDetector()
+
+    while True:
+        ret, frame = cap.read()
+        fingers_are_up = finger_detector.is_palm_extended()
+        print(fingers_are_up)
+        cv2.imshow("Frame", frame)
+        k = cv2.waitKey(1)
+        if k == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
